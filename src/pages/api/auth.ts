7342fef20a419869ab353e92d188df5cda042434
Guilherme Nogueira <guilherme.nogueiras@outlook.com>
2023-03-02 16:45:26 -0300

    first commit

---
+++ src/pages/api/auth.ts
@@ -0,0 +1,24 @@
+import { getAuth, GoogleAuthProvider, FacebookAuthProvider, signInWithPopup } from 'firebase/auth';
+import { firebaseApp } from '../config/firebase';
+
+// Cria as instâncias dos provedores de autenticação do Google e Facebook
+const googleProvider = new GoogleAuthProvider();
+const facebookProvider = new FacebookAuthProvider();
+
+// Cria a instância do objeto de autenticação do Firebase
+const auth = getAuth(firebaseApp);
+
+// Função de login com e-mail e senha
+export const signInWithEmailAndPassword = async (email: string, password: string) => {
+  await signInWithEmailAndPassword(auth, email, password);
+};
+
+// Função de login com Google
+export const signInWithGoogle = async () => {
+  await signInWithPopup(auth, googleProvider);
+};
+
+// Função de login com Facebook
+export const signInWithFacebook = async () => {
+  await signInWithPopup(auth, facebookProvider);
+};
