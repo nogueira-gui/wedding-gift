497f04515a59aaf880180669cd7f75849d9e68ef
Guilherme Nogueira <guilherme.nogueiras@outlook.com>
2023-03-04 11:51:56 -0300

    teste lista item

---
+++ src/component/itemCard.tsx
@@ -0,0 +1,30 @@
+import { PropsWithChildren } from "react";
+
+interface ItemCardProps {
+  image: string;
+  title: string;
+  description: string;
+  quantity: number;
+  selected?: boolean;
+}
+
+export default function ItemCard({
+  image,
+  title,
+  description,
+  quantity,
+  selected = false
+}: PropsWithChildren<ItemCardProps>) {
+  return (
+    <div className={`item-card ${selected ? "selected" : ""}`}>
+      <div className="image-wrapper">
+        <img src={image} alt={title} />
+      </div>
+      <div className="content">
+        <h2>{title}</h2>
+        <p>{description}</p>
+        <span>Quantity: {quantity}</span>
+      </div>
+    </div>
+  );
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,24 +1,13 @@
+import ItemCard from '@/component/itemCard';
 import { useState, useEffect } from 'react';
 import { signInWithGoogle, signInWithFacebook } from '../pages/api/auth'; // Importa funções de autenticação do Firebase
 
 export default function Home() {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-
   const [user, setUser] = useState<string | null>(null);
 
   useEffect(() => {
 
   }, [user]);
-
-
-  const handleEmailChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setEmail(event.target.value);
-  };
-
-  const handlePasswordChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setPassword(event.target.value);
-  };
 
   const handleGoogleLogin = async () => {
     try {
@@ -36,21 +25,99 @@ export default function Home() {
     }
   };
 
-  const handleFormSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-    try {
-      // Chama a função de login com e-mail e senha aqui
-    } catch (error) {
-      console.error(error);
-    }
-  };
+  const items = [{
+    image: "https://picsum.photos/id/237/200/300",
+    title: "Dog",
+    description: "Cachorrinho pretinho bonito",
+    quantity: 1,
+    selected: false
+  },
+  {
+    image: "https://picsum.photos/id/2/200/300",
+    title: "Item 1",
+    description: "Descrição do Item 1",
+    quantity: 5,
+    selected: true
+  },
+  {
+    image: "https://picsum.photos/id/20/200/300",
+    title: "Item 2",
+    description: "Descrição do Item 2",
+    quantity: 1,
+    selected: false
+  },
+  {
+    image: "https://picsum.photos/id/21/200/300",
+    title: "Item 3",
+    description: "Descrição do Item 2",
+    quantity: 1,
+    selected: false
+  },
+  {
+    image: "https://picsum.photos/id/25/200/300",
+    title: "Item 4",
+    description: "Descrição do Item 2",
+    quantity: 1,
+    selected: false
+  },
+  {
+    image: "https://picsum.photos/id/30/200/300",
+    title: "Item 1",
+    description: "Descrição do Item 1",
+    quantity: 5,
+    selected: true
+  },
+  {
+    image: "https://picsum.photos/id/50/200/300",
+    title: "Item 2",
+    description: "Descrição do Item 2",
+    quantity: 1,
+    selected: false
+  },
+  {
+    image: "https://picsum.photos/id/60/200/300",
+    title: "Item 3",
+    description: "Descrição do Item 2",
+    quantity: 1,
+    selected: false
+  },
+  {
+    image: "https://picsum.photos/id/70/200/300",
+    title: "Item 4",
+    description: "Descrição do Item 2",
+    quantity: 1,
+    selected: false
+  }
+  ];
+
 
   return (
     <div>
       {user ? (
-        <p>Bem-vindo, {user}!</p>
+        <>
+          <p>Bem-vindo, {user}!</p>
+          <div style={{
+            display: 'flex',
+            flexWrap: 'wrap',
+            overflow: 'hidden',
+            width:'100vw',
+            maxHeight: '80vh',
+            justifyContent: 'center'
+          }}>
+            {items.map((item) => (
+              <ItemCard
+                key={item.title}
+                image={item.image}
+                title={item.title}
+                description={item.description}
+                quantity={item.quantity}
+                selected={item.selected}
+              />
+            ))}
+          </div>
+        </>
       ) : (
-        <div style={{ backgroundColor: '#444', display: 'flex', flexDirection: 'column', alignItems: 'center' ,padding: '20px', margin:'0px 25px 0px 25px', borderRadius: '10px', boxShadow: '0 0 10px rgba(0, 0, 0, 0.3)' }}>
+        <div style={{ backgroundColor: '#444', display: 'flex', flexDirection: 'column', alignItems: 'center', padding: '20px', margin: '0px 25px 0px 25px', borderRadius: '10px', boxShadow: '0 0 10px rgba(0, 0, 0, 0.3)' }}>
           <div>
             <h1>Acesso</h1>
             <img src="icon.png" alt="Presente Icon" style={{ width: '100px', height: '100px', borderRadius: '50%', marginBottom: '20px' }} />
@@ -60,8 +127,9 @@ export default function Home() {
             <button onClick={handleFacebookLogin}>Sign in with Facebook</button>
           </div>
         </div>
-      )}
-    </div>
+      )
+      }
+    </div >
   );
 }
 
